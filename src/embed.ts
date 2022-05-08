cbe5a9e1c53fec9ecf3f9157f6061be2abe38804
Le_Roux-Nard <65075968+Le-Roux-nard@users.noreply.github.com>
2022-05-08 01:35:16 -0400

    fix html 'code'

--- src/embed.ts
+++ src/embed.ts
@@ -32,12 +32,13 @@ export const generateEmbed = (youtubeVideoUrl: string, pictureLink: string) => {
               /(((//(((((######%%#%                                        
                 //////((/((((((%
 -->
+<head>  
   <meta property="og:type" content="video.other">
-    <meta property="og:video:type" content="text/html">
-    <meta property="og:video:width" content="900">
-    <meta property="og:video:height" content="506">
-    <meta property="og:video:url" content="https://youtube.com/embed/${youtubeVideoUrl}">
-    <meta name="twitter:image" content="${pictureLink}">
-    <meta http-equiv="refresh" content="0;url=https://youtube.com/watch?v=${youtubeVideoUrl}">
-  </head>`;
+  <meta property="og:video:type" content="text/html">
+  <meta property="og:video:width" content="900">
+  <meta property="og:video:height" content="506">
+  <meta property="og:video:url" content="https://youtube.com/embed/${youtubeVideoUrl}">
+  <meta name="twitter:image" content="${pictureLink}">
+  <meta http-equiv="refresh" content="0;url=https://youtube.com/watch?v=${youtubeVideoUrl}">
+</head>`;
 };
