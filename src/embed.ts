f3669a918ca320686b820518a7750e71833fcf18
Le_Roux-Nard <65075968+Le-Roux-nard@users.noreply.github.com>
2022-05-08 01:14:51 -0400

    update embed

--- src/embed.ts
+++ src/embed.ts
@@ -1,12 +1,11 @@
-export const generateEmbed = (youtubeVideoUrl : string, pictureLink : string) => {
+export const generateEmbed = (youtubeVideoUrl: string, pictureLink: string) => {
 	return `<!DOCTYPE html>
   <head>
-  <meta property="og:type" content="video.other">
-    <meta property="og:video:type" content="text/html">
+    <meta property="og:image" content="${pictureLink}">
+    <meta property="og:type" content="video.other">
+    <meta property="og:video:url" content="https://youtube.com/embed/${youtubeVideoUrl}">
     <meta property="og:video:width" content="900">
     <meta property="og:video:height" content="506">
-    <meta property="og:video:url" content="https://youtube.com/embed/${youtubeVideoUrl}">
-    <meta name="twitter:image" content="${pictureLink}">
     <meta http-equiv="refresh" content="0;url=https://youtube.com/watch?v=${youtubeVideoUrl}">
   </head>`;
 };
